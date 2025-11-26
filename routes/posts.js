9b84753dcaeb5bfa1ae5279b2803178502ba01ff
ousemaakacha <akachaousema@gmail.com>
2025-11-26 15:09:57 +0100

    fix: rewrote the routes in post.js using the controller system

--- routes/posts.js
+++ routes/posts.js
@@ -11,33 +11,21 @@ const {
 } = require("../controllers/postscontroller");
 
 // prende la lista di post (index)
-router.get("/", (req, res) => {
-  res.send("Posts List");
-});
+router.get("/", index);
 
 // mi mostra un post specifico (show)
-router.get("/:id", (req, res) => {
-  res.send(`Shows the post number ${req.params.id}`);
-});
+router.get("/:id", get);
 
 // store per un nuovo post
-router.post("/", (req, res) => {
-  res.send("New post here");
-});
+router.post("/", store);
 
 //  aggiorna il post
-router.put("/:id", (req, res) => {
-  res.send(`POst updated with id: ${req.params.id}`);
-});
+router.put("/:id", update);
 
 // modifica il post
-router.patch("/:id", (req, res) => {
-  res.send(`modifies post using id: ${req.params.id}`);
-});
+router.patch("/:id", modify);
 
 // cancella il post
-router.delete("/:id", (req, res) => {
-  res.send(`delete post with id: ${req.params.id}`);
-});
+router.delete("/:id", destroy);
 
 module.exports = router;
